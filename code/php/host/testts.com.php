9f586b8957c1e6e1b488676cce4119602cb6c468
Văn Bằng <congbang10h@gmail.com>
2018-05-21 09:56:02 +0700

    Initial commit

---
+++ code/php/host/testts.com.php
@@ -0,0 +1,8 @@
+<?php
+define('APP_DIR','/Users/vancongbang/Downloads/Database luận văn/ts20160327/code/');
+define('APP_DATA','/Users/vancongbang/Downloads/Database luận văn/ts20160327/data');
+
+define('DB_HOST','localhost:3306');
+define('DB_NAME','testts');
+define('DB_USER','root');
+define('DB_PASS','Bang12345');
